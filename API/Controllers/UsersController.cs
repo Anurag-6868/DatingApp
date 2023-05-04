38f5975b7ffddd20ce76f2d6fb84954bd7b4491c
Anurag <tjboxer14@gmail.com>
2023-05-05 04:37:53 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -35,5 +36,28 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            // var user = await _uow.UserRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            // if (user == null) return NotFound();
+
+            // _mapper.Map(memberUpdateDto, user);
+
+            // if (await _uow.Complete()) return NoContent();
+
+            // return BadRequest("Failed to update user");
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            if (user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update user");
+        }
+
     }
 }
